045ffd57a39596f636e5ac15d24e9fd95ce57fd9
koledanylo <dkolesnychenko@tula.co>
2025-06-07 14:58:29 +0300

    Fix: bind to 0.0.0.0 for Render

--- backend/app.py
+++ backend/app.py
@@ -62,4 +62,5 @@ def send_email(filename, file_url):
         server.send_message(msg)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
